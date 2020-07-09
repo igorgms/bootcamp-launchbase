85aeed476f507dd09772d70a7d6a1a3ac6fc9519
Igor Gomes <igorgmsf@gmail.com>
2020-07-09 18:59:22 -0300

    new file added

---
+++ desafios/desafio1.1Aposentadoria.js
@@ -0,0 +1,17 @@
+const nome = 'Igor'
+const sexo = 'F'
+const idade = 26
+const contribuicao = 35
+
+// Homem pode se aposentar cont 35 e mulher 30
+// cont + idade: Homem 95 e mulher 90
+
+const somaCont = idade + contribuicao
+
+if (sexo == 'M' && (contribuicao >= 35 || somaCont >= 95)){
+    console.log(`${nome} você já pode se aposentar!`)
+} else if (sexo =='F' && (contribuicao >= 30 || somaCont >= 90)){
+    console.log(`${nome} você já pode se aposentar!`)
+} else {
+    console.log(`${nome} você ainda não pode se aposentar!`)
+}
