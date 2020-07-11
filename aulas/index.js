79654ccd85ca71461a0cf3118ac6a688ba5e6f4c
Igor Gomes <igorgmsf@gmail.com>
2020-07-10 23:02:23 -0300

    desafio1.3.1.js added

--- aulas/index.js
+++ aulas/index.js
@@ -1,4 +1,4 @@
-const alunos = [
+const alunosDaTurmaA = [
     {
         nome: 'Igor',
         nota: 9.8
@@ -10,14 +10,55 @@ const alunos = [
     {
         nome: 'Diego',
         nota: 2
+    }, 
+    {
+        nome: 'Murilo',
+        nota: 8
     }
 
 ]
 
-const media = (alunos[0].nota + alunos[1].nota + alunos[2].nota) / alunos.length
+const alunosDaTurmaB = [
+    {
+        nome: 'Igor',
+        nota: 9.8
+    },
+    {
+        nome: 'Julis',
+        nota: 7
+    },
+    {
+        nome: 'Gabriel',
+        nota: 4
+    },
+    {
+        nome: 'Maicon',
+        nota: 5
+    }
 
-console.log(media.toFixed(2))
+]
 
-for (let i = 0; i < alunos.length; i++){
-    console.log(alunos[i].nome, alunos[i].nota)
+function calculaMedia(alunos){
+    let nota = 0
+    for (let i = 0; i < alunos.length; i++){
+            nota += alunos[i].nota
+    }
+
+    const media = nota / alunos.length
+    return media
 }
+
+
+const media1 = calculaMedia(alunosDaTurmaA)
+const media2 = calculaMedia(alunosDaTurmaB)
+
+function enviaMensagem(media){
+    if (media >= 6){
+        console.log(`A média foi de ${media.toFixed(2)}. Parabéns!`)
+    } else {
+        console.log(`A média foi de ${media.toFixed(2)}. Reprovado!`)
+    }
+}
+
+enviaMensagem(media1)
+enviaMensagem(media2)
---
+++ desafios/desafio1.3.1.js
@@ -0,0 +1,14 @@
+const usuarios = [
+    { nome: "Carlos", tecnologias: ["HTML", "CSS"] },
+    { nome: "Jasmine", tecnologias: ["JavaScript", "CSS"] },
+    { nome: "Tuane", tecnologias: ["HTML", "Node.js"] }
+  ]
+
+// for (let i = 0; i < usuarios.length; i++){
+//     console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias}`)
+// }
+
+
+for(let usuario of usuarios){
+    console.log(`${usuario.nome} trabalha com ${usuario.tecnologias}`)
+}
