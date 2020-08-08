58aa3f8730d3895a15f5b4ad9e27a85c0fcc201d
Igor Gomes <igorgmsf@gmail.com>
2020-08-08 15:37:10 -0300

    new files added

--- modulo03/server.js
+++ modulo03/server.js
@@ -15,7 +15,11 @@ nunjucks.configure('views', {
 
 // desta forma o servidor recebe um endereço, e uma função callback que recebe como parÂmetros uma requisição e uma resposta
 server.get('/', function(req, res) {
-    return res.render('index')
+    return res.render('about')
+})
+
+server.get('/portfolio', function(req, res) {
+    return res.render('portfolio')
 })
 
 // servidor está sendo iniciado e o listen é necessário para "escutar" o que está acontecendo dentro do server, são passados como parâmetros o serial da porta e também uma função callback
